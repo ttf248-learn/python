859cf39f51e29b597067b13d2a2db62ff4ed913c
tianlong.xiang <tianlong.xiang@foxmail.com>
2023-06-28 14:07:26 +0800

    feat: 百度飞浆安装脚本

--- 百度飞浆OCR.py
+++ 百度飞浆OCR.py
@@ -1,5 +1,7 @@
 from paddleocr import PaddleOCR, draw_ocr
 
+# pip3 install paddlepaddle
+
 # Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
 # 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
 ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
