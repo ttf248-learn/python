8bd6afd99bb27aee4b6adc261cf00dd7cef79a12
tianlong.xiang <tianlong.xiang@foxmail.com>
2023-06-28 14:03:56 +0800

    feat: 日常更新

--- 字符串转大写.py
+++ 字符串转大写.py
@@ -1,21 +1,10 @@
-# 函数 输入字符串转大写，保存到粘贴板
-# 作者：李宇
-# 日期：2019-12-31
-# 语言：Python 3.7.4
-# 依赖：pyperclip
-# 说明：将输入字符串转大写，保存到粘贴板
-# 用法：将需要转换的字符串复制到剪贴板，运行程序，即可将转换后的字符串保存到剪贴板
+import datetime
 
-import pyperclip
-import time
+init_time = datetime.datetime.strptime("08:50", "%H:%M")
+close_price = datetime.datetime.strptime("13:58", "%H:%M")
 
-text = ''
+init_time_cron = f"{init_time.minute} {init_time.hour} * * 1-5"
+close_price_cron = f"{close_price.minute} {close_price.hour} * * 1-5"
 
-while True:
-    if text != pyperclip.paste() and pyperclip.paste().find('const') == -1:
-        text = pyperclip.paste()
-        pyperclip.copy('const char k' + text.upper() + '[] = "' + text + '";')
-        print('转换成功', text, '->', pyperclip.paste())
-
-    # 休眠1秒
-    time.sleep(1)
+print("Init Time Cron:", init_time_cron)
+print("Close Price Cron:", close_price_cron)
--- 百度飞浆OCR.py
+++ 百度飞浆OCR.py
@@ -3,7 +3,7 @@ from paddleocr import PaddleOCR, draw_ocr
 # Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
 # 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
 ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
-img_path = './imgs/Snipaste_2022-11-30_14-13-38.png'
+img_path = './企业微信截图_16879311346264.png'
 result = ocr.ocr(img_path, cls=True)
 for idx in range(len(result)):
     res = result[idx]
